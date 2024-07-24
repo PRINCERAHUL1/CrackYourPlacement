36a1ddf2c9bb3152a389ec86e80c56d46c663c75
Rahul Mandal <rmrahul258@gmail.com>
2024-07-24 23:30:07 +0530

    Day 9

---
+++ Linked List/Merge Two Sorted Lists.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* temp = new ListNode(0);
+        ListNode* res = temp;
+
+        while(list1 != nullptr && list2 != nullptr)
+        {
+            if(list1->val <= list2->val)
+            {
+                res->next = list1;
+                list1 = list1->next;
+            }
+            else
+            {
+                res->next = list2;
+                list2 = list2->next;
+            }
+            res = res->next;
+        }
+
+        if(list1 != nullptr)
+            res->next = list1;
+        else
+            res->next = list2;
+
+        return temp->next;
+    }
+};
