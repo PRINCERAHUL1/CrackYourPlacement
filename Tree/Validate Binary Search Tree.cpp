b8dc3bd21ad2682a88e231dc9788fef1abc1356b
Rahul Mandal <rmrahul258@gmail.com>
2024-09-09 23:26:57 +0530

    Day 56

---
+++ Tree/Validate Binary Search Tree.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+
+    bool bst(TreeNode* root, long int min, long int max)
+    {
+        if(root==NULL)
+            return true;
+        
+        if(root->val <= min || root->val >=max)
+            return false;
+        
+        return bst(root->left, min, root->val) && bst(root->right, root->val, max);
+    }
+
+    bool isValidBST(TreeNode* root) {
+        return bst(root, LONG_MIN, LONG_MAX);
+    }
+};
