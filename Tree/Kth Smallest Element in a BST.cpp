77e804b8ec86a9d1801a219930627b30d3caa9a4
Rahul Mandal <rmrahul258@gmail.com>
2024-09-12 23:47:05 +0530

    Day 59

---
+++ Tree/Kth Smallest Element in a BST.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        stack<TreeNode*> s;
+
+        while(true)
+        {
+            while(root != NULL)
+            {
+                s.push(root);
+                root=root->left;
+            }
+
+            root=s.top();
+            s.pop();
+            
+            if(--k == 0)
+                return root->val;
+            
+            root=root->right;
+        }
+    }
+};
